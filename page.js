214f1b709fb4484d88afbfe1dab19d750e85e93c
parikshitpandiyaa-cmyk <parikshitpandiyaa@gmail.com>
2026-05-15 09:39:46 +0530

    Add files via upload

---
+++ page.js
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <div style={{background:'black',color:'white',minHeight:'100vh',padding:'40px'}}>
+      <h1 style={{fontSize:'48px',color:'red'}}>Parikshit Pandiya</h1>
+      <p>Cinematic Video Editor Portfolio</p>
+    </div>
+  )
+}
