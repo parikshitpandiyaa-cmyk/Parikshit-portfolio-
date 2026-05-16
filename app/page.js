394c234d6ee7b507348be9847d70c6620f392fb3
parikshitpandiyaa-cmyk <parikshitpandiyaa@gmail.com>
2026-05-16 23:13:32 +0530

    Update page.js

--- app/page.js
+++ app/page.js
@@ -37,7 +37,63 @@ export default function Home() {
           Parikshit Pandiya
         </h1>
 
-        <p style={{ marginTop: "15px" }}>
+       <p
+  style={{
+    color: "#cccccc",
+    fontSize: "18px",
+    marginTop: "20px",
+    lineHeight: "30px"
+  }}
+>
+  I am a cinematic video editor specializing in reels,
+  travel videos, commercial edits, and storytelling content.
+  I create high-quality edits that help creators and brands
+  stand out on social media.
+</p>
+
+<div style={{ marginTop: "25px" }}>
+  <a
+    href="https://instagram.com/parikshit_pandiya"
+    style={{
+      backgroundColor: "red",
+      color: "white",
+      padding: "12px 20px",
+      marginRight: "10px",
+      textDecoration: "none",
+      borderRadius: "10px"
+    }}
+  >
+    Instagram
+  </a>
+
+  <a
+    href="https://wa.me/918107285850"
+    style={{
+      backgroundColor: "red",
+      color: "white",
+      padding: "12px 20px",
+      marginRight: "10px",
+      textDecoration: "none",
+      borderRadius: "10px"
+    }}
+  >
+    WhatsApp
+  </a>
+
+  <a
+    href="mailto:parikshitpandiyaa@gmail.com"
+    style={{
+      backgroundColor: "red",
+      color: "white",
+      padding: "12px 20px",
+      textDecoration: "none",
+      borderRadius: "10px"
+    }}
+  >
+    Email
+  </a>
+</div>
+            <p style={{ marginTop: "15px" }}>
           Cinematic Video Editor
         </p>
 
@@ -97,5 +153,5 @@ export default function Home() {
         </div>
       </div>
     </div>
-  );
-                }
+    );        
+}
